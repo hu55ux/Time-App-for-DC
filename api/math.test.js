c3e589ff897a464d6840131386c0991c357b8bd3
Huseyn <hsbziyev@gmail.com>
2026-05-16 18:29:29 +0400

    Add simple math tests and update workflow to run them

---
+++ api/math.test.js
@@ -0,0 +1,11 @@
+test('adds 1 + 2 to equal 3', () => {
+  expect(1 + 2).toBe(3);
+});
+
+test('multiplies 2 * 3 to equal 6', () => {
+  expect(2 * 3).toBe(6);
+});
+
+test('subtracts 5 - 2 to equal 3', () => {
+  expect(5 - 2).toBe(3);
+});
